de4f599980a821f5c41c11b7a605637be5d6cf0d
Suyog-Kulkarni <98457685+Suyog-Kulkarni@users.noreply.github.com>
2023-04-08 23:40:56 +0530

    Add project files.

---
+++ AuthSystem/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using AuthSystem.Areas.Identity.Data;
+using AuthSystem.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.V4.Pages.Account.Internal;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AuthSystem.Controllers
+{
+    [Authorize]// show home page only to autorize user after logout
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
+        
+        ApplicationUser user = new ApplicationUser();
+
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager)
+        {
+            _logger = logger;
+            this._userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["UserId"] = _userManager.GetUserId(this.User);
+            ViewData["UserName"] = _userManager.GetUserName(this.User);
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
